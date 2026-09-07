8e0726ca0b5dc55c0a706173499547145a507231
Kamal Shelley <kamalshelley@gmail.com>
2026-09-07 14:51:15 +0100

    Deploy Tru Delights to GitHub Pages

---
+++ _next/static/ymrcoQsK7rFdcOSSeFfWW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tru-delights//_next/:path+",
+        "destination": "/tru-delights/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
